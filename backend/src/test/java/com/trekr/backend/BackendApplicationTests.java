72053b5fc3ff291757af1cdd64e8fdfcb9e9b17d
Andrej <asumanovski@yahoo.com>
2026-02-06 22:24:45 +0100

    first commit

---
+++ backend/src/test/java/com/trekr/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trekr.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
